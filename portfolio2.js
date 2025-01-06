b3a708d8a58cd5fb399dd332c888f6804cd0b0dc
Aamir Raza <merciful9795@gmail.com>
2025-01-06 20:34:14 +0530

    minor fix

--- portfolio2.js
+++ portfolio2.js
@@ -1,11 +1,11 @@
-// // toggie icon navbar
-// let menuIcon = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
+// toggie icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-// menuIcon.onclick = () => {
-//   menuIcon.classList.toggle('bx-x');
-//   navbar.classList.toggle('active');
-// };
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
 // // scroll sections avtive link
 // let sections = document.querySelectorAll('section');
 // let navLinks = document.querySelectorAll('header nav a');
